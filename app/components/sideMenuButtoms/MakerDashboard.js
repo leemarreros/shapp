40b777321666a8bb6bdbe38c708cb14f6fb70188
Lee Crosby <lee.marreros@pucp.pe>
2016-01-01 15:14:39 -0800

    [Feature] Implements profile completion line

--- app/components/sideMenuButtoms/MakerDashboard.js
+++ app/components/sideMenuButtoms/MakerDashboard.js
@@ -10,6 +10,7 @@ let {
   Text,
   StyleSheet,
   AlertIOS,
+  ProgressViewIOS,
   TouchableOpacity,
   Image
 } = React;
@@ -19,7 +20,32 @@ export default class MakerDashboard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      progress: 0,
+      widthContainer: 105,
     };
+  }
+
+  componentWillMount() {
+    this.calculateProgress();
+  }
+
+  calculateProgress() {
+    var count = 0;
+    var total = 10;
+    var keys = ['name', 'username', 'fbId','email', 'picture', 'bio' ,'address']
+    for (var key=0;  key < keys.length; key++) {
+      if (keys[key] === 'address') {
+        var addressLength = this.props.userInfo[keys[key]].length-2;
+        count += Math.max(addressLength, 0);
+      } else {
+        if (this.props.userInfo[keys[key]] != undefined) {
+          count++
+        }
+      }
+    }
+    var per = count/total;
+    this.setState({progress: per})
+    this.setState({widthContainer: Math.max(per*window.width, 105)})
   }
 
   render() {
@@ -30,18 +56,26 @@ export default class MakerDashboard extends React.Component {
             <Text style={styles.welcomeText}>Welcome Maker!</Text>
           </View>
           <View style={styles.picture}>
-            <Image style={{width: 146, height: 146 ,borderRadius: 146/2}} source={{uri: this.props.userInfo.picture}}/>
+            <Image resizeMode='cover' style={{width: 146, height: 146 ,borderRadius: 146/2}} source={{uri: this.props.userInfo.picture}}/>
           </View>
           <View style={styles.names}>
             <Text style={styles.namesText}>{this.props.userInfo.name}</Text>
             {this.props.userInfo.userName ?
               <Text style={styles.namesText}>{this.props.userInfo.userName}</Text>
               :
-              <Text style={styles.namesText}>Create a user name</Text>
-            }
+              <Text style={styles.namesText}>Create a user name</Text>}
           </View>
         </View>
-
+        <View style={styles.wrapperProgressView}>
+          <View style={[styles.textProgressContainer, {width: this.state.widthContainer}]}>
+            <Text style={styles.profileCompletionText}>Profile completed {this.state.progress*100}%</Text>
+          </View>
+          <ProgressViewIOS
+            style={{backgroundColor: '#F2F2F2'}}
+            progressTintColor='#50E3C2'
+            progressViewStyle={'bar'}
+            progress={this.state.progress}/>
+        </View>
         <View style={styles.downSide}>
           <TouchableOpacity
             style={styles.buttonDashboard}>
@@ -65,6 +99,19 @@ export default class MakerDashboard extends React.Component {
 }
 
 var styles = StyleSheet.create({
+  wrapperProgressView: {
+    justifyContent: 'flex-end'
+  },
+  textProgressContainer: {
+    justifyContent: 'flex-end',
+  },
+  profileCompletionText: {
+    fontFamily: 'Avenir',
+    fontWeight: '100',
+    fontSize: 10,
+    color: '#285DA1',
+    textAlign: 'right'
+  },
   welcome: {
     flex:1.25 ,
     alignItems: 'center',
